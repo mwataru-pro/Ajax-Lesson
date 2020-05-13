6ef553c81c1880a92697c2f0a8685b433fb0089b
mwataru-pro <m1130wata@gmail.com>
2020-05-13 22:11:28 +0900

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function(){
+	$('#btn').on('click', function(){
+		$.ajax({
+			url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "5b03efcd0c5367047c18f1b502ed34a1",
+			dataType : 'jsonp',
+		}).done(function (data){
+			$('#place').text(data.name);
+			$('#temp_max').text(data.main.temp_max);
+			$('#temp_min').text(data.main.temp_min);
+			$('#humidity').text(data.main.humidity);
+			$('#speed').text(data.wind.speed);
+			$('#weather').text(data.weather[0].main);
+			$('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+			$('img').attr("alt",data.weather[0].main);
+		}).fall(function (data){
+			alert('通信に失敗しました。')
+		});
+	});
+});
